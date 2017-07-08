8786275e3ecb58b24653aae7f8a2f800f6352505
yamapi06150 <yamapi06150@gmail.com>
2017-07-09 03:42:27 +0800

    complete basic mongodb mondel/entity setting

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const dbConnectionString = require('./config/config').dbConnectionString;
+const CommentModel = require('./models/CommentModel');
 
 mongoose.connect(dbConnectionString);
 const db = mongoose.connection;
 db.on('error', (error) => { console.log('connection failed ', error); });
 db.once('open', () => {
-    console.log('connection successed');
+  console.log('connection successed');
+
+  let testComment = new CommentModel({
+    from: 'alvin',
+    commentValue: 'test123',
+  }).save((err, result) => {
+    if (err) { console.log('testComment save failed：', err); }
+    console.log('testComment save successed：', result);
+  });
 });
 
 let app = express();
---
+++ models/CommentModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    from: { type: String },
+    commentValue: { type: String },
+  },
+  { timestamps: true } // 千萬不要加逗號...eslint bug...
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
